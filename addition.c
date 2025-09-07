3c964789c5ac741697a62218c712fef8f44466d4
arya-prog410 <aryaa.verma410@gmail.com>
2025-09-07 22:43:44 +0530

    addition

--- addition.c
+++ addition.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 
-int main(){
-    int num1, num2, sum;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &num1, &num2);
-    sum=num1+num2;
-    printf("Addition of %d and %d is %d",num1,num2,sum);
+int main()
+{
+    int a,b;
+    printf("enter a: ");
+    scanf("%d",&a);
+
+    printf("enter b: ");
+    scanf("%d",&b);
+
+    int sum=a+b;
+    printf("%d",sum);
 
     return 0;
 }
