e37db827b0f6c663094d3750a13c7b55ff2ebd4b
arya-prog410 <aryaa.verma410@gmail.com>
2025-09-07 20:35:52 +0530

    variable comments

---
+++ variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main ()
+{
+    // defining variables
+ /*
+ hello multiline 
+ comments
+ */
+    int age= 18;
+    float pi= 3.14;
+    char star='*';
+    printf(age,pi,star);
+    return 0;
+}
