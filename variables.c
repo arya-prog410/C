93c2deeca146be4043ca04881b7f735b4869d6f4
arya-prog410 <aryaa.verma410@gmail.com>
2025-09-07 20:41:23 +0530

    multiline

--- variables.c
+++ variables.c
@@ -7,9 +7,13 @@ int main ()
  hello multiline 
  comments
  */
+printf("hello c \n");
+printf("this is second line \n");
+printf("this is third line \n");
+printf("this is third line\n");
     int age= 18;
     float pi= 3.14;
     char star='*';
-    printf(age,pi,star);
+    // printf(age,pi,star);
     return 0;
 }
