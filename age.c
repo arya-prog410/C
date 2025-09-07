ceb0397dd77c05d3fe0085c1b2cdc1d6459bce4c
arya-prog410 <aryaa.verma410@gmail.com>
2025-09-07 20:57:32 +0530

    used variables

---
+++ age.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int age=18;
+    printf("age is %d \n", age);
+
+    float pi=3.14;
+    printf("pi is %.2f \n",pi);
+
+    char star='*';
+    printf("char is %c \n",star);
+    return 0;
+}
